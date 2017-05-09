cf384cbe480e03a224ddf6fad0a49ec14cc98d8c
yoeunes <younes.khoubza@gmail.com>
2017-05-10 00:45:03 +0100

    a function that helps to remove accent from any string

--- helpers.php
+++ helpers.php
@@ -16,3 +16,15 @@ if (!function_exists('dd')) {
         die();
     }
 }
+
+if (!function_exists('remove_accents')) {
+    function remove_accents($str, $charset = 'utf-8')
+    {
+        $str = htmlentities($str, ENT_NOQUOTES, $charset);
+        $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+        $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str);
+        $str = preg_replace('#&[^;]+;#', '', $str);
+
+        return $str;
+    }
+}
