4fba6118f04219028bacc59043474a43f078f3a2
yoeunes <younes.khoubza@gmail.com>
2017-05-10 00:45:42 +0100

    get xml and html function

--- helpers.php
+++ helpers.php
@@ -28,3 +28,30 @@ if (!function_exists('remove_accents')) {
         return $str;
     }
 }
+
+if (!function_exists('getXML')) {
+    function getXML($url = '')
+    {
+        return new SimpleXMLElement(file_get_contents($url));
+    }
+}
+
+if (!function_exists('getHtml')) {
+    function getHtml($url, $post = null)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        if (!empty($post)) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        }
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+    }
+}
