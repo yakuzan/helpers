489486a893e023be021ab47dfe3bb92117ad6e91
yoeunes <younes.khoubza@gmail.com>
2017-05-10 00:44:18 +0100

    adding dump and dd functions

--- helpers.php
+++ helpers.php
@@ -1,1 +1,18 @@
 <?php
+
+if (!function_exists('dump')) {
+    function dump($data = [])
+    {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
+}
+
+if (!function_exists('dd')) {
+    function dd($data = [])
+    {
+        dump($data);
+        die();
+    }
+}
