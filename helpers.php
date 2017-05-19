b1614a82820eff6c2fad132a1912ac2da7bdfd21
yoeunes <younes.khoubza@gmail.com>
2017-05-19 09:36:23 +0100

    adding array_has method

--- helpers.php
+++ helpers.php
@@ -226,3 +226,33 @@ if ( ! function_exists('array_get')) {
         return $array;
     }
 }
+
+if ( ! function_exists('array_has')) {
+    /**
+     * Check if an item exists in an array using "dot" notation.
+     *
+     * @param  array $array
+     * @param  string $key
+     *
+     * @return bool
+     */
+    function array_has(array $array, $key) {
+        if (empty($array) || $key === null) {
+            return false;
+        }
+
+        if (array_key_exists($key, $array)) {
+            return true;
+        }
+
+        foreach (explode('.', $key) as $segment) {
+            if ( ! is_array($array) || ! array_key_exists($segment, $array)) {
+                return false;
+            }
+
+            $array = $array[$segment];
+        }
+
+        return true;
+    }
+}
