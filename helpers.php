6069e6e3ed7c090e05c20e1fd81f9483e47807da
yoeunes <younes.khoubza@gmail.com>
2017-05-19 09:45:43 +0100

    adding array_dot method

--- helpers.php
+++ helpers.php
@@ -430,3 +430,28 @@ if ( ! function_exists('array_reset')) {
         return $target;
     }
 }
+
+
+if ( ! function_exists('array_dot')) {
+    /**
+     * Flatten a multi-dimensional associative array with dots.
+     *
+     * @param  array $array
+     * @param  string $prepend
+     *
+     * @return array
+     */
+    function array_dot(array $array, $prepend = '') {
+        $results = [];
+
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                $results = array_merge($results, array_dot($value, $prepend . $key . '.'));
+            } else {
+                $results[$prepend . $key] = $value;
+            }
+        }
+
+        return $results;
+    }
+}
